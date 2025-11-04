87296af56484d41e1da7face7df8ba5566a6a9a2
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-04 15:07:10 +0100

    feat:Completato il primo esercizio

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function capitalize(array) {
+    newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const parola = array[i];
+        //console.log(parola);
+        const primaLettera = parola[0].toUpperCase();
+        newArray.push(primaLettera);
 
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(capitalize(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
