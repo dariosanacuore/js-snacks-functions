91173b9d38900d6cc97802edb7ac93233bd78b66
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-04 15:14:38 +0100

    feat:Completato il secondo esercizio

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function letteraIniziale(array) {
+    newArray = [];
+    let lettera = "A";
+    for (let i = 0; i < array.length; i++) {
+        const parola = array[i];
+        //console.log(parola);
+        const primaLettera = parola[0];
+        if (primaLettera === lettera) {
+            newArray.push(parola);
+        }
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(letteraIniziale(names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
